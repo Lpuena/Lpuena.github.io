c26ba0cfe3c02f19d8c0d8d2c532dd1d4692d554
Lpuena <lpuena@outlook.com>
2025-02-09 09:27:09 +0800

    refactor(nuxt.config.ts): 更新favicon图标路径

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,8 +21,8 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        // { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', href: '/carbonApplicationWeb.png' },
       ],
     },
   },
